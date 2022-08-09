d6af3ac740a6d2355671fb18c1cde6edea26763a
PixelJ3 <ger.reyes258@gmail.com>
2022-08-09 16:08:44 -0700

    return the given parameters in a full sentence

--- sayHello.js
+++ sayHello.js
@@ -1,7 +1,7 @@
 /*
 Create a method sayHello/say_hello/SayHello that takes as input a name, city, and state to welcome a person. 
 Note that name will be an array consisting of one or more values that should be joined together with one space 
-between each, and the length of the name array in test cases will vary
+between each, and the length of the name array in test cases will vary.
 
 P: you will be given an array with a first and last name.
 R: return the given parameters in a full sentence
