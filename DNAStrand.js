76a98a31dc1bd4439313c0fff54f712819589916
PixelJ3 <ger.reyes258@gmail.com>
2022-08-09 13:37:31 -0700

    replace DNA strand 'A' and 'T', 'C' and 'G' swapped

--- DNAStrand.js
+++ DNAStrand.js
@@ -6,7 +6,7 @@ functioning of living organisms.
 If you want to know more: http://en.wikipedia.org/wiki/DNA
 
 In DNA strings, symbols "A" and "T" are complements of each other, as 
-"C" and "G". You function receives one side of the DNA (string, except for Haskell); 
+"C" and "G". Your function receives one side of the DNA (string, except for Haskell); 
 you need to return the other complementary side. DNA strand is never 
 empty or there is no DNA at all (again, except for Haskell).
 
@@ -19,7 +19,7 @@ DNAStrand("ATTGC"),"TAACG","String ATTGC is"
 DNAStrand("GTAT"),"CATA","String GTAT is"
 P: 
 create a function that takes in a string
-then replace 'A' with 'T' and 'C' with 'G' and visevera
+then replace 'A' with 'T' and 'C' with 'G' and vise versa
 then return the string
 
 */
