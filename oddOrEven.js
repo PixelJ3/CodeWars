65f7e17ee384a451226d2ba1f4a0436b2a46bfff
PixelJ3 <ger.reyes258@gmail.com>
2022-08-09 15:39:06 -0700

    return "odd" or "even"

--- oddOrEven.js
+++ oddOrEven.js
@@ -1,19 +1,8 @@
 /*
 Given a list of integers, determine whether the sum of its elements is odd or even.
-
 Give your answer as a string matching "odd" or "even".
 
 If the input array is empty consider it as: [0] (array with a zero).
-
-Examples:
-Input: [0]
-Output: "even"
-
-Input: [0, 1, 4]
-Output: "odd"
-
-Input: [0, -1, -5]
-Output: "even"
 
 P: you will be given an array of numbers positive and negative
 R: return "odd" or "even" if the sum is odd or even
