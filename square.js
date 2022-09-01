26dd355bd37eaf65546365dbbb7a9fc0e08dd761
PixelJ3 <ger.reyes258@gmail.com>
2022-08-31 18:26:44 -0700

    create a function that takes an argument and returns the square of it.

---
+++ square.js
@@ -0,0 +1,23 @@
+/* 
+Now you have to write a function that takes an argument and returns the square of it.
+
+P: you will be given a random number 
+R: return the given number squared
+E: 
+    square(3) //expected output: 9
+    square(5); //expected output: 25
+    square(12); //expected output: 144
+P:  
+create a function that takes in a number
+return number squared using math.square()
+
+*/
+
+const square = (number) => Math.pow(number, 2);
+
+square(3); //expected output: 9
+square(5); //expected output: 25
+square(12); //expected output: 144
+square(9); //expected output: 81
+square(2); //expected output: 4
+square(10); //expected output: 100
