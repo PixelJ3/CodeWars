662877c316669b320bfa9dae483db429f3b09354
PixelJ3 <ger.reyes258@gmail.com>
2022-09-26 10:17:22 -0700

    return the array in a reversed order

---
+++ reverseList.js
@@ -0,0 +1,21 @@
+/*
+create a function that takes in a list and returns a list with the reverse order.
+
+
+
+P: you will be given an array 
+R: return the array in a reversed order
+E: 
+ [1, 2, 3, 4]  -> [4, 3, 2, 1]
+ [9, 2, 0, 7]  -> [7, 0, 2, 9]
+P:
+create a funciton that takes in an array
+use reverse method on array
+return arr 
+*/
+function reverseList(list) {
+  return list.reverse();
+}
+
+//one line:
+const reverseList = (list) => list.reverse();
