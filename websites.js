8aac21c01ad28e924fef22d6efa771303cc4cd4b
PixelJ3 <ger.reyes258@gmail.com>
2022-10-06 10:40:01 -0700

    find a way to store 'codewars' in the given variable without creating a new array

---
+++ websites.js
@@ -0,0 +1,21 @@
+/* 
+Add the value "codewars" to the websites array.
+After your code executes the websites array should == ["codewars"]
+
+The websites array has already been defined for you using the following code:
+
+var websites = [];
+
+
+
+P: you will be given a variable named websites with an empty array 
+R: find a way to store 'codewars' in the given variable without creating a new array
+E: 
+    var websites = []; => var websites = ['codewars'];
+P:
+we already have an array created so theres no need to create one
+using push, we can push 'codewars' into the websites array
+*/
+
+var websites = [];
+websites.push("codewars");
