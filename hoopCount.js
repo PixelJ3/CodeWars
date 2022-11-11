4633c0161f07390989eeccc0d270161e51762b52
PixelJ3 <ger.reyes258@gmail.com>
2022-11-11 12:12:18 -0800

    Write a program where Alex can input (n) how many times the hoop goes round and it will return him an encouraging message :)

---
+++ hoopCount.js
@@ -0,0 +1,28 @@
+/*
+Alex just got a new hula hoop, he loves it but feels discouraged because his little brother is better than him
+
+Write a program where Alex can input (n) how many times the hoop goes round and it will return him an encouraging message :)
+
+If Alex gets 10 or more hoops, return the string "Great, now move on to tricks".
+If he doesn't get 10 hoops, return the string "Keep at it until you get it".
+
+P: you will be given a number
+R: Write a program where Alex can input (n) how many times the hoop goes round and it will return him an encouraging message :)
+E: 
+n < 10, return "Keep at it until you get it"
+n >= 10, return  "Great, now move on to tricks"
+P:
+create a function that takes in a number
+if the number is less than 10
+return "Keep at it until you get it".
+if the number is 10 or greater
+return "Great, now move on to tricks".
+*/
+
+function hoopCount(n) {
+  if (n < 10) {
+    return "Keep at it until you get it";
+  } else {
+    return "Great, now move on to tricks";
+  }
+}
