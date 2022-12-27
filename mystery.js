237247b6c9e9c40036a83ad83ab5a093660c2835
PixelJ3 <ger.reyes258@gmail.com>
2022-12-26 21:47:18 -0800

    create a function that returns the Object when its called

---
+++ mystery.js
@@ -0,0 +1,17 @@
+/*
+This function should return an object, but it's not doing what's intended. What's wrong?
+
+P: you will be given a function that needs to be debugged 
+R: create a function that returns the Object when its called
+E:
+P:
+create a function
+create an object and store it within a variable
+return the variable when its called
+*/
+function mystery() {
+  var results = { sanity: "Hello" };
+  return results;
+}
+
+mystery(); //return output: {sanity: "Hello"}
