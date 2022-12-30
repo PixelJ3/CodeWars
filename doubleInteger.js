60c5ad74395499b1214f7d08b90fae6cef923845
PixelJ3 <ger.reyes258@gmail.com>
2022-12-29 22:51:41 -0800

    return the given number doubled

---
+++ doubleInteger.js
@@ -0,0 +1,15 @@
+/*
+Code as fast as you can! You need to double the integer and return it.
+
+P: you will be given a number
+R: return the given number doubled
+E: 
+(1 + 1) => 2
+P:
+create a function that takes in a number
+return n + n
+*/
+function doubleInteger(i) {
+  // i will be an integer. Double it and return it.
+  return i + i;
+}
